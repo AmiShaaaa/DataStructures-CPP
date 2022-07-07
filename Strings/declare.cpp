68b12c0c97ebd631c8230e79dbf8ad60f518e123
Amisha <amishass2002@gmail.com>
2022-07-07 22:14:46 +0530

    strings

---
+++ Strings/declare.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Declare string:
+
+    // 1.
+    // string str;
+    // cin>>str;
+    // cout<<str<<endl;
+
+    // 2.
+    // string str1(5, 'n');
+    // cout<<str1<<endl;
+
+    // 3.
+    // string str = "AmishaSharma";
+    // cout<<str<<endl;
+
+    // Input with spaces: 
+
+    // string str;
+    // getline(cin, str);
+    // cout<<str<<endl;
+    
+    return 0;
+}
---
+++ Strings/strFunc.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Functions in string -->
+    // 1. Append function : To join two or more strings/words.
+    string s1 = "fam";
+    string s2 = "ily";
+
+    // cout << "Before using append function: " << s1 + s2 << endl;
+
+    // M-1: s1 mein s2 append:
+    // s1.append(s2);
+    // cout << s1 << endl;
+
+    // M-2: without appending:
+    // cout << "After using append function: " << s1 + s2 << endl;
+
+    // Accessing letter from string -->
+    // cout << s1[1] << endl;
+    // cout << s2[1] << endl;
+
+    // Clear function -->
+    // string abc = "Hi hello how do you do?";
+    // cout<<abc;
+    // abc.clear();
+    // cout<<abc;
+    
+    return 0;
+}
