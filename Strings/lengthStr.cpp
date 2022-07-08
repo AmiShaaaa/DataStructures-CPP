8ff137d86cbbd932cf5cf75dfe1edad052cf3a1c
Amisha <amishass2002@gmail.com>
2022-07-08 23:12:53 +0530

    strings

--- Strings/lengthStr.cpp
+++ Strings/lengthStr.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    string s1 = "Amisha";
-    // cout << s1.size() << endl;
-    // cout << s1.length() << endl;
-
-    for (int i = 0; i < s1.length(); i++)
-        cout << s1[i] << endl;
-    return 0;
+int getLength(char name[]){
+    int count = 0;
+    for(int i = 0; name[i]!='\0'; i++)
+        count++;
+    return count;
 }
+
+int main(){
+
+    char name[20];
+    cin>>name; 
+    cout<<getLength(name)<<endl; 
+}
+
+// int main()
+// {
+//     string s1 = "Amisha";
+//     // using method -->
+//     // cout << s1.size() << endl;
+//     // cout << s1.length() << endl;
+
+//     for (int i = 0; i < s1.length(); i++)
+//         cout << s1[i] << endl;
+//     return 0;
+// }
